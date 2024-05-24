50285d4422ffaad0066f29ad265275f015291a34
jonas pauli <jonaspauli089@gmail.com>
2024-05-25 00:14:07 +0200

    remove useless verifier function

--- api/src/verifier.rs
+++ api/src/verifier.rs
@@ -3,15 +3,9 @@ use risc0_types::CircuitOutputs;
 use k256::{
     ecdsa::{signature::{Signer, Verifier}, Signature, SigningKey, VerifyingKey}, EncodedPoint
 };
-fn verify_receipt(receipt: Receipt, program_id: [u32;8]) -> CircuitOutputs{
+pub fn verify_receipt(receipt: Receipt, program_id: [u32;8]) -> CircuitOutputs{
     receipt.verify(program_id).expect("Failed to verify proof");
     receipt.journal.decode().expect("Failed to extract public journal from receipt")
-}
-
-pub fn verify_receipt_against(receipt: Receipt, program_id: [u32;8], government_public_key: VerifyingKey) -> CircuitOutputs{
-    let journal = verify_receipt(receipt, program_id);
-    assert_eq!(VerifyingKey::from_encoded_point(&EncodedPoint::from_bytes(&journal.government_public_key).expect("Failed to parse as encoded point")).expect("Failed to parse as verifying key"), government_public_key);
-    journal
 }
 
 // todo: perform an additional check that journal.public_identity is in gov_sigs: Vec<todo!("sigs")>
