c043152c2da7d0ef1b462b7e091408c12b2d13da
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-05-25 00:24:37 +0200

    methods: format

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,21 +1,41 @@
-use risc0_zkvm::guest::env;
-use risc0_types::{CircuitInputs, CircuitOutputs};
 use k256::{
-    ecdsa::{signature::{Signer, Verifier}, Signature, SigningKey, VerifyingKey}, EncodedPoint
+    ecdsa::{
+        signature::{Signer, Verifier},
+        Signature, SigningKey, VerifyingKey,
+    },
+    EncodedPoint,
 };
+use risc0_types::{CircuitInputs, CircuitOutputs};
+use risc0_zkvm::guest::env;
 fn main() {
     let circuit_inputs: CircuitInputs = env::read();
     let choice: String = circuit_inputs.choice;
-    let user_public_key: VerifyingKey = VerifyingKey::from_encoded_point(&EncodedPoint::from_bytes(&circuit_inputs.user_public_key).unwrap()).unwrap();
-    let government_public_key: VerifyingKey = VerifyingKey::from_encoded_point(&EncodedPoint::from_bytes(&circuit_inputs.government_public_key).unwrap()).unwrap();
+    let user_public_key: VerifyingKey = VerifyingKey::from_encoded_point(
+        &EncodedPoint::from_bytes(&circuit_inputs.user_public_key).unwrap(),
+    )
+    .unwrap();
+    let government_public_key: VerifyingKey = VerifyingKey::from_encoded_point(
+        &EncodedPoint::from_bytes(&circuit_inputs.government_public_key).unwrap(),
+    )
+    .unwrap();
 
-    user_public_key.verify(&circuit_inputs.government_public_key, &circuit_inputs.session_signature).expect("Failed to verify session signature");
-    government_public_key.verify(&circuit_inputs.user_public_key, &circuit_inputs.public_identity).expect("Failed to verify public identity");
+    user_public_key
+        .verify(
+            &circuit_inputs.government_public_key,
+            &circuit_inputs.session_signature,
+        )
+        .expect("Failed to verify session signature");
+    government_public_key
+        .verify(
+            &circuit_inputs.user_public_key,
+            &circuit_inputs.public_identity,
+        )
+        .expect("Failed to verify public identity");
 
-    let output: CircuitOutputs = CircuitOutputs{
+    let output: CircuitOutputs = CircuitOutputs {
         choice: choice,
         government_public_key: circuit_inputs.government_public_key,
-        public_identity: circuit_inputs.public_identity
+        public_identity: circuit_inputs.public_identity,
     };
     env::commit(&output);
 }
