83d01fd43de563abc747be6a159be69c6c70cfd8
jonas pauli <jonaspauli089@gmail.com>
2024-05-24 21:18:46 +0200

    risc0 api

---
+++ risc0-types/src/lib.rs
@@ -0,0 +1,21 @@
+use k256::{
+    ecdsa::{signature::{Signer, Verifier}, Signature, SigningKey, VerifyingKey},
+    EncodedPoint, PublicKey,
+};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct CircuitInputs{
+    pub choice: String,
+    pub user_public_key: Vec<u8>, 
+    pub user_secret_key: Vec<u8>, 
+    pub government_public_key: Vec<u8>, 
+    pub public_identity: Vec<u8>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CircuitOutputs{
+    pub choice: String,
+    pub government_public_key: Vec<u8>,
+    pub public_identity: Vec<u8>
+}
