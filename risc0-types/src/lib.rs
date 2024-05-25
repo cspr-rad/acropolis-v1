2ccfdd20d17643bb3b582e748eac30600400d268
jonas pauli <jonaspauli089@gmail.com>
2024-05-25 11:17:15 +0200

    add function to deserialize the government public key in CircuitOutputs

--- risc0-types/src/lib.rs
+++ risc0-types/src/lib.rs
@@ -1,5 +1,5 @@
-use k256::ecdsa::Signature;
 use serde::{Deserialize, Serialize};
+use k256::{ecdsa::{signature::Signer, Signature, SigningKey, VerifyingKey}, EncodedPoint};
 
 #[derive(Serialize, Deserialize)]
 pub struct CircuitInputs {
@@ -15,4 +15,10 @@ pub struct CircuitOutputs {
     pub choice: String,
     pub government_public_key: Vec<u8>,
     pub public_identity: Signature,
+}
+
+impl CircuitOutputs{
+    pub fn deserialized_government_public_key(&self) -> VerifyingKey{
+        VerifyingKey::from_encoded_point(&EncodedPoint::from_bytes(&self.government_public_key).unwrap()).unwrap()
+    }
 }
