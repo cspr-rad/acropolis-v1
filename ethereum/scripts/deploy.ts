8fa339eaa2dc7f1174eb80ad3b04e5dcb39c195f
Matthew Doty <matt@w-d.org>
2024-05-25 18:38:41 +0200

    Fixing formatting

--- ethereum/scripts/deploy.ts
+++ ethereum/scripts/deploy.ts
@@ -1,20 +1,19 @@
 import fs from "fs";
 import { ethers } from "hardhat";
-import "@nomicfoundation/hardhat-toolbox";
 
 const main = async (): Promise<void> => {
-    const voteRecorderFactory  = await ethers.getContractFactory("VoteRecorder");
+    const voteRecorderFactory = await ethers.getContractFactory("VoteRecorder");
     console.log("Deploying VoteRecorder...");
     const voteRecorder = await voteRecorderFactory.deploy();
     await voteRecorder.waitForDeployment();
     const voteRecorderAddress = await voteRecorder.getAddress();
     console.log(`VoteRecorder address: ${voteRecorderAddress}`);
     fs.writeFileSync(`${__dirname}/../CONTRACT_ADDRESS`, voteRecorderAddress);
-} 
+}
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
--- ethereum/scripts/vote.ts
+++ ethereum/scripts/vote.ts
@@ -3,19 +3,19 @@ import { ethers } from "hardhat";
 import { VoteRecorder } from "../typechain-types";
 
 const main = async (): Promise<void> => {
-    const voteRecorderFactory  = await ethers.getContractFactory("VoteRecorder");
+    const voteRecorderFactory = await ethers.getContractFactory("VoteRecorder");
     const voteRecorder = voteRecorderFactory.attach(
         fs.readFileSync(`${__dirname}/../CONTRACT_ADDRESS`, 'utf8')
     ) as VoteRecorder;
     const electionId = process.env["ELECTION_ID"] as string;
     const voteData = process.env["VOTE_DATA"] as string;
     await voteRecorder.vote(electionId, voteData);
-    console.log(await voteRecorder.getVotesTallied(electionId))
+    console.log(`${(await voteRecorder.getVotesTallied(electionId)).toString()} vote(s) cast`)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
